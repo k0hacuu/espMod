9b97ac8ffb3cf6a626f262454976ce690e1853ff
Jonny Yang <jiamu@outlook.com>
2022-06-12 12:24:51 +0100

    Replaced placeholders

--- src/main/java/com/SunnyCN/BanditEsp/BanditEsp.java
+++ src/main/java/com/SunnyCN/BanditEsp/BanditEsp.java
@@ -1,20 +1,21 @@
-package com.example.examplemod;
+package com.SunnyCN.BanditEsp;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = BanditEsp.MODID, version = BanditEsp.VERSION)
+public class BanditEsp
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "BanditEsp";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
+
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        
     }
 }
